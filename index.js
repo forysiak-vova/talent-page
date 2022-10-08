2d9fbfda9b47763879215ce1fe978487a5da6059
forysiak-vova <volodja.forus@gmail.com>
2022-10-09 00:31:27 +0200

    add page teams

---
+++ index.js
@@ -0,0 +1,29 @@
+document.querySelector(".burger").addEventListener("click", () => {
+  const htmlString = `
+   <span class="close-icon"></span>
+   <ul>
+      <li>
+         <a href="#" title="Home">Home</a>
+      </li>
+      <li>
+         <a href="#" title="Portfolio">Services</a>
+      </li>
+      <li>
+         <a href="#" title="Services">About Us</a>
+      </li>
+      <li>
+         <a href="#" title="Contact">Log in</a>
+      </li>
+   </ul>
+ `;
+
+  const modal = document.createElement("div");
+  modal.className = "modal";
+  modal.innerHTML = htmlString;
+  document.body.append(modal);
+  document.body.classList.add(".document.body");
+  document.querySelector(".close-icon").addEventListener("click", (e) => {
+    modal.remove();
+    document.body.classList.remove(".document.body");
+  });
+});
