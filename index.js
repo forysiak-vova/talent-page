8889f9bab75a04ff393840ae5c7dd471834ff4e2
forysiak-vova <volodja.forus@gmail.com>
2022-10-09 10:53:18 +0200

    fix footer and container

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ document.querySelector(".burger").addEventListener("click", () => {
   modal.className = "modal";
   modal.innerHTML = htmlString;
   document.body.append(modal);
-  document.body.classList.add(".document.body");
+  document.body.classList.add(".visually-hidden");
   document.querySelector(".close-icon").addEventListener("click", (e) => {
     modal.remove();
-    document.body.classList.remove(".document.body");
+    document.body.classList.remove(".visually-hidden");
   });
 });
